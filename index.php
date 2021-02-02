a79950a1c7dfe0a40d17f0b202dc74387d899d37
GUILHERME ANDRE GARCIA BUENO <987301152@itaud.des.ihf>
2021-02-02 14:04:45 -0300

    atualizando com os divs

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<h1 style="color: red"> Teste site Gui</h1>]
-<h2 style="color: red"> Alteracao feita</h2>]
+<h1 style="color: red"> Teste site Gui</h1>
+<h2 style="color: red"> Alteracao feita</h2>
 
 <div style="display:flex; height:400px"> 
-<div sytle="background-color:blue">
-    essa eh a coluna de cor azul
-</div>
-<div sytle="background-color:yellow">
-    essa eh a coluna de cor amarela
-</div>
+    <div sytle="background-color:blue">
+        essa eh a coluna de cor azul
+    </div>
+    <div sytle="background-color:yellow">
+        essa eh a coluna de cor amarela
+    </div>
 </div>
