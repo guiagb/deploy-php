e64abd2276a7ab69aa0e21acd30f449015a891d2
GUILHERME ANDRE GARCIA BUENO <987301152@itaud.des.ihf>
2021-02-02 14:01:34 -0300

    atualizando com os divs

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <h1 style="color: red"> Teste site Gui</h1>]
 <h2 style="color: red"> Alteracao feita</h2>]
 
-<div style="display:flex height:400px"> 
+<div style="display:flex; height:400px"> 
 <div sytle="background-color:blue">
     essa eh a coluna de cor azul
 </div>
